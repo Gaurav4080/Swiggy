427936efcf5b6d01c9ff18b8de0b50d38c2ada9c
Gaurav Agarwal <aggarwalgaurav353@gmail.com>
2025-02-21 12:38:39 +0530

    Code for Cusines Component done.

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -7,42 +7,39 @@ function Head() {
 
         <div className='flex items-center'>
           <img className='w-24' src="https://1000logos.net/wp-content/uploads/2021/05/Swiggy-emblem.png" alt="" />
-          
+
           <div className='flex items-center gap-1'>
             <p className='font-bold border-b-2 border-black'>others</p>
-            <i class="fi text-1.5xl text-orange-500 mt-2 fi-rr-angle-small-down"></i>
+            <i className="fi text-1.5xl text-orange-500 mt-2 fi-rr-angle-small-down"></i>
           </div>
         </div>
 
         <div className='flex items-center gap-9'>
           <div className='flex '>
-          <i class="fi fi-rr-shopping-bag"></i>
-          <p>Swiggy Corporate</p>
+            <i className="fi fi-rr-shopping-bag"></i>
+            <p>Swiggy Corporate</p>
           </div>
           <div className='flex '>
-          <i class="fi fi-rr-shopping-bag"></i>
-          <p>Search</p>
+            <i className="fi fi-rr-shopping-bag"></i>
+            <p>Search</p>
           </div>
           <div className='flex '>
-          <i class="fi fi-rr-shopping-bag"></i>
-          <p>Offers</p>
+            <i className="fi fi-rr-shopping-bag"></i>
+            <p>Offers</p>
           </div>
           <div className='flex '>
-          <i class="fi fi-rr-shopping-bag"></i>
-          <p>Help</p>
+            <i className="fi fi-rr-shopping-bag"></i>
+            <p>Help</p>
           </div>
           <div className='flex '>
-          <i class="fi fi-rr-shopping-bag"></i>
-          <p>Sign In</p>
+            <i className="fi fi-rr-shopping-bag"></i>
+            <p>Sign In</p>
           </div>
           <div className='flex '>
-          <i class="fi fi-rr-shopping-bag"></i>
-          <p>Cart</p>
+            <i className="fi fi-rr-shopping-bag"></i>
+            <p>Cart</p>
           </div>
         </div>
-
-
-
       </div>
     </div>
   )
