94a56cac29953900085dfa1d07f070d7dd74be39
Gaurav Agarwal <aggarwalgaurav353@gmail.com>
2025-03-15 17:37:03 +0530

    Strat of implementing Location Search functionlaity

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,24 @@ import { Route, Routes } from "react-router-dom"
 import Body from "./components/Body"
 import Head from "./components/Head"
 import RestaurantMenu from "./components/RestaurantMenu"
+import { Visibility } from "./context/contextApi"
+import { useState } from "react"
 
 function App() {
 
+    const [visible, setVisible] = useState(false)
+
   return (
-    <Routes>
-      <Route path="/" element={<Head />}>
-        <Route path="/" element={<Body />}></Route>
-        <Route path="/restaurantMenu/:id" element={<RestaurantMenu />}></Route>
-      </Route>
-    </Routes>
+    <Visibility.Provider value={{visible, setVisible}}>
+      <div className={visible ? "max-h-screen overflow-hidden" : ""}>
+        <Routes>
+          <Route path="/" element={<Head />}>
+            <Route path="/" element={<Body />}></Route>
+            <Route path="/restaurantMenu/:id" element={<RestaurantMenu />}></Route>
+          </Route>
+        </Routes>
+      </div>
+    </Visibility.Provider>
   )
 }
 
--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link, Outlet } from 'react-router-dom'
+import { Visibility } from '../context/contextApi'
 
 function Head() {
 
@@ -30,16 +31,33 @@ function Head() {
     }
   ]
 
+  const { visible, setVisible } = useContext(Visibility)
+
+  function handleVisbility() {
+    setVisible(prev => !prev)
+  }
+
   return (
-    <>
-      <div className='w-full shadow-md h-24 flex justify-center items-center'>
+    <div className='relative w-full'>
+
+      <div className='w-full'>
+        <div onClick={handleVisbility} className={"w-full bg-black/50 h-full z-30 absolute " + (visible ? "visible " : "invisible")}></div>
+        <div className={'bg-white w-[40%] h-full absolute z-40 duration-500 ' + (visible ? "left-0" : "-left-[100%]")}>
+          <p className='bg-black text-white p-5 w-[10%]' onClick={handleVisbility}>Cut</p>
+        </div>
+      </div>
+
+
+
+
+      <div className='w-full sticky bg-white z-20 top-0 shadow-md h-18 flex justify-center items-center'>
         <div className=' w-[70%] flex justify-between'>
 
           <div className='flex items-center'>
             <Link to={"/"}>
               <img className='w-24' src="https://1000logos.net/wp-content/uploads/2021/05/Swiggy-emblem.png" alt="" />
             </Link>
-            <div className='flex items-center gap-1'>
+            <div className='flex items-center gap-1' onClick={handleVisbility}>
               <p className='font-bold border-b-2 border-black'>others</p>
               <i className="fi text-1.5xl text-orange-500 mt-2 fi-rr-angle-small-down"></i>
             </div>
@@ -57,8 +75,9 @@ function Head() {
           </div>
         </div>
       </div>
+
       <Outlet />
-    </>
+    </div>
   )
 }
 
